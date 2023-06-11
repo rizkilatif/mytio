7d3c751d713245e3203c45d0760ec564605b9bae
rizkilatif <rizkilatifyt12@gmail.com>
2023-06-11 15:16:13 +0700

    implemen routing

---
+++ app/Controllers/Page.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+class Page extends BaseController
+{
+    public function about()
+    {
+        echo "About Tio";
+    }
+    public function contact()
+    {
+        echo "Contact Tio";
+    }
+    public function faqs()
+    {
+        echo "Faqs Tio";
+    }
+    public function tos()
+    {
+        echo "Tos Tio";
+    }
+}
